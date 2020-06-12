83e30e04abe3c9ba8aa632de53cb23864a301330
kumar-bit <kumaranandr12@gmail.com>
2020-06-12 22:20:44 +0530

    employee status

---
+++ Uc1.java
@@ -0,0 +1,11 @@
+public class Uc1{
+public static void main(String args[]){
+int  fulltime=1;
+double empcheck=Math.floor(Math.random()*10) % 2;
+if (empcheck==fulltime)
+System.out.println("employee is present");
+else
+System.out.println("employee is absent");
+}
+}
+
